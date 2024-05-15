b50979631dda66809bc2cb332884bb3be747b0ef
unknown <eruffini@myges.fr>
2024-05-15 18:45:00 +0200

    change .py

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
-Base = declarative_base()
+#Base = declarative_base()
 
 class Log(Base):
     __tablename__ = 'logs'
